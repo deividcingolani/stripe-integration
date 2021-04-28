ee5f59224388478422943492ff0fbba710a3a3cc
David Cingolani <cingolanidavid@gmail.com>
2021-04-28 16:52:28 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from './CheckoutForm';
+
+const STRIPE_PUBLISHABLE_KEY = 'pk_test_51IlIoYBhf7mFz8SCJYmdkhuvvxidJBeu47KbOGaySQD60WkNLu2Q3MfWfgvGJF1blZaPKNCHHBE7Vfwzub0lw9zG00trRoTh7Z';
+
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Elements stripe={stripePromise}>
+            <CheckoutForm />
+        </Elements>
+    );
 }
 
 export default App;
