fc3fbb68589a844956c573ea5039f3a83bfd2091
David Cingolani <cingolanidavid@gmail.com>
2021-05-03 14:56:43 -0300

    NOW IT WORKS

--- src/api/test.js
+++ src/api/test.js
@@ -1,27 +1,21 @@
 const Stripe = require('stripe');
-const stripe = Stripe('sk_test_51IlIoYBhf7mFz8SCqbBVBUk0WtAuHVW4Y14jZHJYR8kemiWvLWVawAHJDCwL5U4j36ri3rReEp45p3Z6aD6NM13M00Cq1s9TsP');
+const SECRET = 'sk_test_51IlIoYBhf7mFz8SCqbBVBUk0WtAuHVW4Y14jZHJYR8kemiWvLWVawAHJDCwL5U4j36ri3rReEp45p3Z6aD6NM13M00Cq1s9TsP';
+const stripe = new Stripe(SECRET);
 
-const pay = async (req, res) => {
-    // var paymentIntent;
-    // try {
-    //     paymentIntent = await stripe.paymentIntents.create({
-    //         amount: 200,
-    //         currency: "usd",
-    //         payment_method_types: ["card"],
-    //         receipt_email: "cingolanidavid@gmail.com",
-    //     });
-    //     console.log(paymentIntent);
-    // } catch (error) {
-    //     console.log(error);
-    //     process.exit(1);
-    // }
+const pay = async () => {
+    // const { id, amount } = req.body
+    const id = 'pm_1In5zYBhf7mFz8SCttkkRmBk';
     try {
-        const paymentConfirm = await stripe.paymentIntents.confirm(
-            'pm_1In2mHBhf7mFz8SCWq0gXVDZ',
-            { payment_method: "pm_card_visa" }
-        );
-        console.log('------------------------------------------------------------');
-        console.log(paymentConfirm);
+        const payment = await stripe.paymentIntents.create({
+            // Multiply amount * 100
+            amount: 200 * 100, //req.body
+            currency: 'usd',
+            description: 'Describe the product',
+            payment_method: id, //req.body
+            confirm: true
+        });
+    
+        console.log(payment);
     } catch (error) {
         console.log(error);
     }
